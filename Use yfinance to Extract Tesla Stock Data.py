8252a2720ccc13d39286e707717a55fef83603e1
Bharath838 <117983582+Bharath838@users.noreply.github.com>
2023-10-20 20:49:17 +0530

    Update Use yfinance to Extract Tesla Stock Data.py

--- Use yfinance to Extract Tesla Stock Data.py
+++ Use yfinance to Extract Tesla Stock Data.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+%matplotlib inline
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
